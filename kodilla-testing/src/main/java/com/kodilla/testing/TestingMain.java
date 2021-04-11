f58fc8c23aa447f1f8a584929ebe5c0991abe606
Janusz <mail.janusz@wp.pl>
2021-04-11 20:11:58 +0200

    6.3 Added OddNumbersExterminator class with tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,35 +6,6 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
 
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        // Calculator test
-
-        int a= 100;
-        int b= 200;
-        int addResult =a+b;
-        int subtractResult =a-b;
-
-        Calculator calculator=new Calculator();
-
-        if(calculator.add(a,b) == addResult){
-            System.out.println("Test OK");
-        }else{
-            System.out.println("ERROR!");
-        }
-
-        if(calculator.subtract(a,b) == subtractResult){
-            System.out.println("Test OK");
-        }else{
-            System.out.println("ERROR!");
-        }
     }
 }
