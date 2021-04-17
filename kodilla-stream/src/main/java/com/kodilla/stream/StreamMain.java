e45c16dd54bffa7034ee26106439e785be6af748
Janusz <mail.janusz@wp.pl>
2021-04-17 18:28:06 +0200

    7.2 ForumUser class in com.kodilla.stream.immutable package to check.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,8 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
@@ -11,5 +13,9 @@ public class StreamMain {
         poemBeautifier.beautify("-DEFG-", stringToDecorate -> "ABC" + stringToDecorate + "ABC");
         poemBeautifier.beautify("What a sunny Day!", stringToDecorate -> "~ ".concat(stringToDecorate).concat(" ~"));
         poemBeautifier.beautify("java is better than Haskell", stringToDecorate -> stringToDecorate.substring(0, 4).toUpperCase().concat("!"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
     }
 }
