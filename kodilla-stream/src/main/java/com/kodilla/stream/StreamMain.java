4385f131f8a22228faaad3bb75df713bb95f2308
Janusz <mail.janusz@wp.pl>
2021-04-18 21:07:14 +0200

    7.3  Package com.kodilla.stream.forumuser and StreamMain class to check.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,21 +1,26 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        poemBeautifier.beautify("Hello World ;-)", stringToDecorate -> stringToDecorate.toUpperCase());
-        poemBeautifier.beautify("-DEFG-", stringToDecorate -> "ABC" + stringToDecorate + "ABC");
-        poemBeautifier.beautify("What a sunny Day!", stringToDecorate -> "~ ".concat(stringToDecorate).concat(" ~"));
-        poemBeautifier.beautify("java is better than Haskell", stringToDecorate -> stringToDecorate.substring(0, 4).toUpperCase().concat("!"));
+        Forum listOfUsers = new Forum();
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        Map<Integer, ForumUser> filteredListOfUsers = listOfUsers.getUsersList().stream()
+                .filter(user -> user.getSex() == 'M')
+                .filter(user -> user.getBirthday().getYear() >= 20)
+                .filter(user -> user.getNumberOfPosts() > 0)
+                .collect(Collectors.toMap(ForumUser::getIdentifier, user -> user));
 
+
+        filteredListOfUsers.entrySet().stream()
+                .map(entry -> " Identifier: " + entry.getKey() + ",  value:" + entry.getValue())
+                .forEach(System.out::println);
     }
 }
