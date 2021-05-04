caff032cec09509641b0d8206a0edf5c2dcff3f8
Janusz <mail.janusz@wp.pl>
2021-05-04 16:04:03 +0200

    RPS game - complete project.

---
+++ kodilla-rps/src/main/java/com/codilla/rps/EqualChancesVersion/RpsRunner.java
@@ -0,0 +1,11 @@
+package com.codilla.rps.EqualChancesVersion;
+
+public class RpsRunner {
+
+    public static void main(String[] args) {
+
+        RpsGameCore newGame = new RpsGameCore();
+        newGame.startNewGame();
+
+    }
+}
