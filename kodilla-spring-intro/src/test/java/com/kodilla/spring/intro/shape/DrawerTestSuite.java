b6dc646b6528572fe9d0f8f09de9bd3ea860aa91
Janusz <mail.janusz@wp.pl>
2021-06-21 17:50:27 +0200

    14.1 DI, IoC

--- kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DrawerTestSuite {
 
     @Test
+<<<<<<< HEAD
     void testDoDrawing() {
         //Given
         Drawer drawer = new Drawer();
@@ -16,5 +17,30 @@ class DrawerTestSuite {
 
         //Then
         //do nothing
+=======
+    void testDoDrawingWithCircle() {
+        //Given
+        Circle circle = new Circle();
+
+        //When
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+
+        //Then
+        assertEquals("This is a circle", result);
+    }
+
+    @Test
+    void testDoDrawingWithTriangle() {
+        //Given
+        Triangle triangle = new Triangle();
+
+        //When
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+
+        //Then
+        assertEquals("This is a triangle", result);
+>>>>>>> 4205745 (14.1 DI, IoC)
     }
 }
