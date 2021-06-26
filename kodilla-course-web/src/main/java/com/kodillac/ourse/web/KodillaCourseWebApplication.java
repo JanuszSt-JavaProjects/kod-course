88a524090094355641aee7f6fc719e1191885ede
Janusz <mail.janusz@wp.pl>
2021-06-26 20:33:43 +0200

    First Commit on branch WEB

---
+++ kodilla-course-web/src/main/java/com/kodillac/ourse/web/KodillaCourseWebApplication.java
@@ -0,0 +1,13 @@
+package com.kodillac.ourse.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseWebApplication.class, args);
+    }
+
+}
