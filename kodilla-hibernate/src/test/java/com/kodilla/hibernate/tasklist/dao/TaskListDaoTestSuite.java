ddf3c6e1f851c40d9f7d88f4a44895f150a721e3
Janusz <mail.janusz@wp.pl>
2021-07-13 21:52:41 +0200

    17.2 Task CORRECTLY !

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -5,18 +5,35 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class TaskListDaoTestSuite {
 
+
     @Autowired
     TaskListDao taskListDao;
 
     @Test
-    void testFindByListName() {
+    void testFindByListName() throws SQLException {
 
         //Given
+        Connection conn;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "kodilla_user");
+        connectionProps.put("password", "kodilla_Pass123");
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" +
+                        "&useSSL=False",
+                connectionProps);
+
+
         String listName = "User1 Tasks";
         String description = "Tasks of J. Rambo";
         TaskList testedList = new TaskList(listName, description);
@@ -28,6 +45,7 @@ public class TaskListDaoTestSuite {
         assertEquals(testedList, taskListDao.findByListName(listName));
 
         //Cleanup
-        taskListDao.deleteAll();
+        Statement statement = conn.createStatement();
+        statement.execute("drop table tasklists");
     }
 }
